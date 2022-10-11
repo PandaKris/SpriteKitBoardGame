430d2a719237d6ea8edb9888066a1623805500e5
PandaKris <kristantosean@gmail.com>
2022-10-11 13:36:24 +0700

    Initial Commit

---
+++ SpriteKitBoardGame/SpriteKitBoardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitBoardGameApp.swift
+//  SpriteKitBoardGame
+//
+//  Created by Kristanto Sean N on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitBoardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
